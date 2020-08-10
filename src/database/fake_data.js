0f234777946fa6996396f0a4c38a040be782ae6c
D4rkAngel <gabrielbalbino@hotmail.com>
2020-08-09 22:34:37 -0300

    Next Level Week#2

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+// Dados
+const proffys = [
+    { name: "Gabriel Balbino", 
+      avatar: "https://avatars3.githubusercontent.com/u/68253786?s=460&u=297331430632c0937f2b54ebbe177788a9e7b096&v=4", 
+      whatsapp: "89946464", 
+      bio: "Entusiasta das melhores magias de artes das tevas.<br><br>Apaixonado por magias proibidas e exerocismos de demônios poderosos do quinto piso do inferno. Mais de 200.000 demônios já foram exorcizados.", 
+      subject: "Artes das Trevas", 
+      cost: "666", 
+      weekday: [0], 
+      time_from: [720], 
+      time_to:[1220]
+    },
+
+    { name: "Lucifer Morningstar", 
+    avatar: "https://avatars2.githubusercontent.com/u/52131891?s=460&u=6071a04fa37bc08a7af3b998b5e198b7e4c1e3a9&v=4", 
+    whatsapp: "89946464", 
+    bio: "Entusiasta das melhores magias de artes das tevas.<br><br>Apaixonado por magias proibidas e exerocismos de demônios poderosos do quinto piso do inferno. Mais de 200.000 demônios já foram exorcizados.", 
+    subject: "Magias Infernais", 
+    cost: "666", 
+    weekday: [1], 
+    time_from: [720], 
+    time_to:[1220]
+  }
+]
